0367c9d3da0c8e1ee993449b5284295703d40a61
WarisLec <warisibrahimathhamulla.h@asc.srmtrichy.edu.in>
2024-11-29 15:47:30 +0530

    Create UsernameProcessor.java

---
+++ UsernameProcessor.java
@@ -0,0 +1,10 @@
+public class UsernameProcessor {
+    public static String processUsername(String username) {
+        // Logic for processing username goes here
+        return ""; // Return the expected output
+    }
+
+    public static void main(String[] args) {
+        System.out.println(processUsername("InputHere")); // Replace InputHere for testing
+    }
+}
